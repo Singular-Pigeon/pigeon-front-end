4ff1f2862bd0115449e253b30ccdf223eb11d45c
chollingsworth <caleb.hollingsworth10@gmail.com>
2020-09-25 13:11:59 -0500

    added complete functionality

--- script.js
+++ script.js
@@ -20,7 +20,8 @@ const triviaQuestions = [
 	'What is the maximum capacity of a floppy disk?',
 	'What Scottish poet’s works inspired the book titles Of Mice and Men and Catcher in the Rye?',
 ];
-const triviaAnswers = [
+//correct answer bank
+const triviaCorrectAnswerBank = [
 	'Bird',
 	'2',
 	'254',
@@ -38,7 +39,81 @@ const triviaAnswers = [
 	'Michael Jackson',
 	'Claude Monet',
 	'Fang',
-    'Steamboat Willie',
-    '2.8 megabytes'
+	'Steamboat Willie',
+	'2.8 megabytes',
 	'Robert Burns',
 ];
+//full bank of answers
+const triviaFullAnswerBank = [
+	['Bird'],
+	['2'],
+	['254'],
+	['James Brown'],
+	['Obama'],
+	['Nacho Cheese'],
+	['Mississippi River'],
+	['Belgium'],
+	['A bowling alley'],
+	['Classical era'],
+	['Shadowfax'],
+	['Michael Scarn'],
+	['Princess Leia'],
+	['Blue Whale'],
+	['Michael Jackson'],
+	['Claude Monet'],
+	['Fang'],
+	['Steamboat Willie'],
+	['2.8 megabytes'],
+	['Robert Burns'],
+];
+//specific index
+let triviaIndex = 0;
+//check answer function
+function checkAnswer(button) {
+	if (button.innerHTML === triviaCorrectAnswerBank[triviaIndex]) {
+		triviaIndex++;
+		console.log(triviaIndex);
+		response.innerHTML = 'Correct! Your Pigeon grows stronger!';
+		resetButton();
+	} else {
+		return (response.innerHTML =
+			"WRONGO! Let's give that another go, little fella.");
+	}
+}
+//query selectors
+const question = document.querySelector('.trivia-question');
+let button1 = document.querySelector('.trivia1');
+let button2 = document.querySelector('.trivia2');
+let button3 = document.querySelector('.trivia3');
+let button4 = document.querySelector('.trivia4');
+let response = document.querySelector('.trivia-response');
+let index = document.querySelector('.trivia-index');
+
+question.innerHTML = triviaQuestions[triviaIndex];
+button1.innerHTML = triviaFullAnswerBank[triviaIndex][0];
+button2.innerHTML = triviaFullAnswerBank[triviaIndex][1];
+button3.innerHTML = triviaFullAnswerBank[triviaIndex][2];
+button4.innerHTML = triviaFullAnswerBank[triviaIndex][3];
+index.innerHTML = triviaIndex;
+//innerHTML setting
+function resetButton() {
+	question.innerHTML = triviaQuestions[triviaIndex];
+	button1.innerHTML = triviaFullAnswerBank[triviaIndex][0];
+	button2.innerHTML = triviaFullAnswerBank[triviaIndex][1];
+	button3.innerHTML = triviaFullAnswerBank[triviaIndex][2];
+	button4.innerHTML = triviaFullAnswerBank[triviaIndex][3];
+	index.innerHTML = triviaIndex;
+}
+//button event listeners
+button1.addEventListener('click', function () {
+	checkAnswer(button1);
+});
+button2.addEventListener('click', function () {
+	checkAnswer(button2);
+});
+button3.addEventListener('click', function () {
+	checkAnswer(button3);
+});
+button4.addEventListener('click', function () {
+	checkAnswer(button4);
+});
